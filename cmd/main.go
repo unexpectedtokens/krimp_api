e90717a411eb791af6b49aff86d617e1f15f91f8
Daniel de Jong <dtronicdevs@icloud.com>
2022-01-09 12:12:51 +0100

    after fasdk

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	firebase "firebase.google.com/go/v4"
+
+	"google.golang.org/api/option"
+)
+
+func initateFirebaseAdminSDK() (*firebase.App, error) {
+	opt := option.WithCredentialsFile("./krimp-43829-firebase-adminsdk-gc8wp-705d896b03.json")
+	app, err := firebase.NewApp(context.Background(), nil, opt)
+	if err != nil {
+		return nil, fmt.Errorf("error initializing app: %v", err)
+	}
+
+	return app, nil
+}
 
 func main() {
-	fmt.Println("yes")
+	app, err := initateFirebaseAdminSDK()
+	if err != nil {
+		panic(err)
+	}
+	client, _ := app.Auth(context.Background())
+	client.VerifyIDToken(context.Background(), "asdasdasdw1231aqsd")
 }
